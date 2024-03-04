43a7c9bbc38a3fc73b6aea11b905e0588b9bb639
Tyler Rhoads <tyrhoads@gmail.com>
2024-03-03 20:18:56 -0500

    finished

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,15 @@
 ## practice python tutorial
 
-person = "tyler"
-print ("subcribe to " + person)
-print ("subscirbe to {}".format(person))
-print(f"subscibe to {person}")
+##person = "tyler"
+##print ("subcribe to " + person)
+##print ("subscirbe to {}".format(person))
+##print(f"subscibe to {person}")
+
+adj = input("Adjective: ")
+verb = input("Verb:  ")
+verb2 = input("verb:  ")
+famous_person = input(" famopus person: ")
+madlib = f"computer programing is so {adj}! It makes me so happy to\
+{verb}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
